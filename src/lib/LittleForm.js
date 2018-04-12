3e6e4c074a8eb940f7fea8a51c61772b5bdab8b8
zetlen <zetlen@gmail.com>
2018-04-12 12:05:45 -0500

    fix warnings

--- src/lib/LittleForm.js
+++ src/lib/LittleForm.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const inputTypes = {
   select: (data, config) => (
-    <select className="browser-default" name={config.name}>
+    <select className="browser-default" key={config.name} name={config.name}>
       {data[config.source].map(record => (
         <option key={record[config.valueKey]} value={record[config.valueKey]}>
           {record[config.labelKey]}
@@ -14,7 +14,7 @@ const inputTypes = {
 
 export default class LittleForm extends React.Component {
   render() {
-    return <div>{this.props.fields.map(f => this.renderInput(f))}</div>;
+    return <span>{this.props.fields.map(f => this.renderInput(f))}</span>;
   }
   renderInput(field) {
     return inputTypes[field.type](this.props.data, field);
